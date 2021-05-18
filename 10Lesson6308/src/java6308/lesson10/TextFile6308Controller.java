147cc0200177b04d15b33d1242e644724b01cf8b
Master <git@github.com:mmjserwer/ddjzxd.git>
2021-05-18 15:22:59 +0800

    默认的

--- 10Lesson6308/src/java6308/lesson10/TextFile6308Controller.java
+++ 10Lesson6308/src/java6308/lesson10/TextFile6308Controller.java
@@ -1,4 +1,34 @@
 package java6308.lesson10;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class TextFile6308Controller {
+
+    @FXML
+    private Label lblSource;
+
+    @FXML
+    private Label lblTarget;
+
+    @FXML
+    private TextArea taText;
+
+    @FXML
+    void copyFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectSource(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectTarget(ActionEvent event) {
+
+    }
+
 }
