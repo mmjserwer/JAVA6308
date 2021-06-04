006d6e116e0faa3af04ff2982af2f43738045875
Master <git@github.com:mmjserwer/ddjzxd.git>
2021-06-04 17:42:08 +0800

    默认的

--- 12Lesson6308/src/java6308/task/Counter6308.java
+++ 12Lesson6308/src/java6308/task/Counter6308.java
@@ -2,20 +2,17 @@ package java6308.task;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Counter6308 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String[] split = s.split("");
-        Map<String,Integer> map=new TreeMap<>();
-        /*Arrays.asList(split).stream().collect(Collectors.groupingBy(String::new, TreeMap::new, Collectors.counting())).forEach(
-                (k, v) -> {
-                    System.out.println(k+"=="+v);
+        Map<String, Long> collect = Arrays.asList(split).stream().collect(Collectors.groupingBy(String::new,Collectors.counting()));
+        collect.keySet().stream().sorted(Comparator.comparing(s1->collect.get(s1)).reversed()).forEach(
+                k->{
+                    System.out.println(k+"="+collect.get(k));
                 }
-        );*/
-
-        Map<String, List<String>> cc = Arrays.asList(split).stream().collect(Collectors.groupingBy(String::new));
+        );
     }
 }
